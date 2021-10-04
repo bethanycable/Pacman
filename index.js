c9adef7d6e56f33c21c58ff877343d742aaef2fd
bethanycable <67919858+bethanycable@users.noreply.github.com>
2021-10-04 18:54:55 +0000

    created controls function to listen for key codes.

--- index.js
+++ index.js
@@ -69,3 +69,22 @@ function createBoard() {
 }
 
 createBoard();
+
+//Pacman starting position
+let pacmanCurrentIndex =  490;
+
+squares[pacmanCurrentIndex].classList.add('pacman');
+
+function control(e) {
+  if(e.keyCode === 40) {
+    console.log('pressed down');
+  } else if(e.keyCode === 39) {
+    console.log('pressed down');
+  } else if(e.keyCode === 38) {
+    console.log('pressed down');
+  } else if(e.keyCode === 37) {
+    console.log('pressed down');
+  }
+
+}
+document.addEventListener('keyup', control)
